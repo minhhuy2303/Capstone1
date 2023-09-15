6f5078419b42c54e521130fda2fc89acb78dfe57
minhhuy2303 <www.prolahuyday@gmail.com>
2023-09-15 20:51:25 +0700

    home patient

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { ComponentsComponent } from './shared/components/components.component';
+import { HeaderComponent } from './shared/common/header/header.component';
+import { FooterComponent } from './shared/common/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PatientComponent } from './shared/components/patient/patient.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PatientComponent,
+    ComponentsComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
