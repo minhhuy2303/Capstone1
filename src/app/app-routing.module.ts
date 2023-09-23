75038cdc04fa5ed7210c2638d88fc1ed66cb90fe
minhhuy2303 <www.prolahuyday@gmail.com>
2023-09-23 10:05:29 +0700

    pull code dat lich

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './shared/components/login/login.component';
 import { DoctorDetailsComponent } from './shared/components/doctor/doctor-details/doctor-details.component';
+import { PatientRecordComponent } from './shared/components/patient/patient-record/patient-record.component';
 
 const routes: Routes = [
   {path:'', pathMatch:'full', redirectTo:"home"},
   {path:"home", component:HomeComponent},
   {path:"login", component:LoginComponent},
-  {path:"info", component:DoctorDetailsComponent}
+  {path:"info", component:DoctorDetailsComponent},
+  {path:"profile", component:PatientRecordComponent}
 ];
 
 @NgModule({
